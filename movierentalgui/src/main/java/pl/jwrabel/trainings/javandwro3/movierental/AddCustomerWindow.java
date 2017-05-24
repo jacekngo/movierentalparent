a16321ffef3d350d2a2ddd520e391f3abfa56e96
jacek <jacekngo@gmail.com>
2017-05-24 21:00:35 +0200

    hi

--- movierentalgui/src/main/java/pl/jwrabel/trainings/javandwro3/movierental/AddCustomerWindow.java
+++ movierentalgui/src/main/java/pl/jwrabel/trainings/javandwro3/movierental/AddCustomerWindow.java
@@ -1,6 +1,7 @@
 package pl.jwrabel.trainings.javandwro3.movierental;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 
 /**
  * Created by RENT on 2017-05-24.
@@ -32,8 +33,44 @@ public class AddCustomerWindow extends JFrame {
         txtSurname.setLocation(80, 50);
         add(txtSurname);
 
+        JLabel pesel = new JLabel("Pesel");
+        pesel.setSize(300, 30);
+        pesel.setLocation(10, 90);
+        add(pesel);
 
-    }
+        JTextField txtPesel = new JTextField();
+        txtPesel.setSize(200, 30);
+        txtPesel.setLocation(80, 90);
+        add(txtPesel);
+
+        JLabel city = new JLabel("Miasto");
+        city.setSize(300, 30);
+        city.setLocation(10, 130);
+        add(city);
+
+        JTextField txtCity = new JTextField();
+        txtCity.setSize(200, 30);
+        txtCity.setLocation(80, 130);
+        add(txtCity);
+
+        JButton addCustomerButton = new JButton("Dodaj klienta");
+        addCustomerButton.setSize(150, 40);
+        addCustomerButton.setLocation(80, 180);
+        add(addCustomerButton);
+
+        JTextField message = new JTextField();
+        message.setSize(350, 300);
+        message.setLocation(10, 250);
+        add(message);
+
+        addCustomerButton.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String firstName = txtName.getText();
+
+            }
+
+    });}
 
     public static void main(String[] args) {
     new AddCustomerWindow();
